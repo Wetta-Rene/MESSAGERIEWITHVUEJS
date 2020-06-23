9cf057dcf2b4f418890833999b74cc10c6ce7e2c
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-06-23 11:59:41 +0200

    no message

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,12 +3,12 @@
     <div id="infoConnexionToUseApi">Vous devez être connecté.e pour utiliser l'application !</div>
       <ConnexionComponent />
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 import ConnexionComponent from '@/components/ConnexionComponent.vue'
-
 
 export default {
   name: 'InscriptionView',
